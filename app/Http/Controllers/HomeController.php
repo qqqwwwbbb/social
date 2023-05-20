f20e9de6e08cdf0cc5429ed149d1ed1b4ac45ad7
qqqwwwbbb <usecaut@gmail.com>
2023-05-21 01:08:53 +0700

    вывод записей на стене

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Status;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        #метод pluck извлекает все значения по заданному ключу, у нас тут вывод новостей либо вывод новостей друзей.
+        #пагинация 10 сортировка по дате DESC
         if ( Auth::check() ) {
-            return view('timeline.index');
+            $statuses = Status::where(function($query){
+                return $query->where('user_id', Auth::user()->id)
+                    ->orWhereIn('user_id', Auth::user()->friends()->pluck('id'));
+            })
+            ->orderBy('created_at','desc')
+            ->paginate(3);
+
+            return view('timeline.index', compact('statuses'));
         }
 
         return view('home');
--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -22,8 +22,46 @@
     </div>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-6"><hr>
 
+        @if ( ! $statuses->count() )
+            <p>Никто ещё не опубликовал запись :(</p>
+        @else
+         @foreach($statuses as $status)
+            <div class="media">
+        <a class="mr-3" href="{{ route('profile.index', ['username'=>$status->user->username]) }}">
+         <img class="media-object rounded" src="{{ $status->user->getAvatarUrl() }}"
+              alt="{{ $status->user->getNameOrUsername() }}">
+        </a>
+        <div class="media-body">
+        <h4>
+            <a href="{{ route('profile.index', ['username'=>$status->user->username]) }}">{{ $status->user->getNameOrUsername() }}</a>
+        </h4>
+            <p>{{ $status->body }}</p>
+        <ul class="list-inline">
+{{--  Приятный вид diffForHumans вместо обычной длинной даты!  --}}
+            <li class="list-inline-item">{{ $status->created_at->diffForHumans()}}</li>
+            <li class="list-inline-item">
+            <a href="#">Лайк</a>
+            </li>
+         <li class="list-inline-item">10 Лайков</li>
+        </ul>
+
+            <form method="POST" action="#" class="mb-4">
+                @csrf
+                <div class="form-group">
+                    <textarea name="status" class="form-control"
+                              placeholder="Прокомментировать" rows="3"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary btn-sm" value="Ответить">
+            </form>
+        </div>
+            </div>
+
+          @endforeach
+
+             {{ $statuses->links() }}
+        @endif
         </div>
     </div>
 
